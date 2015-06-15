10001cd57ad773f110c3ccef3dbab662009eca24
Tan Si Nan <tansinan_1995@163.com>
2015-06-15 18:53:32 +0800

    Add Randomization Feature To Button Module
    Now use button click time to set random seed.

--- Snake 3D/Sources/Button.c
+++ Snake 3D/Sources/Button.c
@@ -37,7 +37,7 @@ void Button_init()
 	GPIOE_PDDR &= ~(1<<4);
 	GPIOE_PDDR &= ~(1<<5);
 	
-	Timer_set(10, Button_onTimer);
+	Timer_set(5, Button_onTimer);
 	
 	for(int i=0;i<8;i++)
 	{
@@ -51,6 +51,7 @@ void Button_onTimer()
 	{
 		if(keyStatus[KEY_UP] == 0)
 		{
+			srand(rand()&(Timer_getSystemTime()+17771));
 			App_sendMessage(EVENT_KEY_DOWN, KEY_UP);
 		}
 		keyStatus[KEY_UP] = 1;
@@ -64,6 +65,7 @@ void Button_onTimer()
 	{
 		if(keyStatus[KEY_LEFT] == 0)
 		{
+			srand(rand()&(Timer_getSystemTime()+41991));
 			App_sendMessage(EVENT_KEY_DOWN, KEY_LEFT);
 		}
 		keyStatus[KEY_LEFT] = 1;
@@ -77,6 +79,7 @@ void Button_onTimer()
 	{
 		if(keyStatus[KEY_DOWN] == 0)
 		{
+			srand(rand()&(Timer_getSystemTime()+17991));
 			App_sendMessage(EVENT_KEY_DOWN, KEY_DOWN);
 		}
 		keyStatus[KEY_DOWN] = 1;
@@ -90,6 +93,7 @@ void Button_onTimer()
 	{
 		if(keyStatus[KEY_RIGHT] == 0)
 		{
+			srand(rand()&(Timer_getSystemTime()+14891));
 			App_sendMessage(EVENT_KEY_DOWN, KEY_RIGHT);
 		}
 		keyStatus[KEY_RIGHT] = 1;
@@ -103,6 +107,7 @@ void Button_onTimer()
 	{
 		if(keyStatus[KEY_A] == 0)
 		{
+			srand(rand()&(Timer_getSystemTime()+17291));
 			App_sendMessage(EVENT_KEY_DOWN, KEY_A);
 		}
 		keyStatus[KEY_A] = 1;
@@ -116,6 +121,7 @@ void Button_onTimer()
 	{
 		if(keyStatus[KEY_B] == 0)
 		{
+			srand(rand()&(Timer_getSystemTime()+60991));
 			App_sendMessage(EVENT_KEY_DOWN, KEY_B);
 		}
 		keyStatus[KEY_B] = 1;
@@ -129,6 +135,7 @@ void Button_onTimer()
 	{
 		if(keyStatus[KEY_C] == 0)
 		{
+			srand(rand()&(Timer_getSystemTime()+33413));
 			App_sendMessage(EVENT_KEY_DOWN, KEY_C);
 		}
 		keyStatus[KEY_C] = 1;
@@ -142,6 +149,7 @@ void Button_onTimer()
 	{
 		if(keyStatus[KEY_D] == 0)
 		{
+			srand(rand()&(Timer_getSystemTime()+17991));
 			App_sendMessage(EVENT_KEY_DOWN, KEY_D);
 		}
 		keyStatus[KEY_D] = 1;
